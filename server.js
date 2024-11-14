8666a3a6ce598dc941378d565b60262a2dc2771a
jeremymayo <jeremydmayo@gmail.com>
2024-11-14 10:07:35 -0600

    adding users.csv

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3333;
 
 // Middleware
 app.use(cors({
